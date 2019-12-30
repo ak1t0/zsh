ca965453c81d076a71ab6e647f1052485bbde357
ak1t0 <aktoo3097@gmail.com>
2019-12-30 22:52:23 +0900

    initial commit

---
+++ config.zsh
@@ -0,0 +1,10 @@
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.zsh_history
+
+alias co="git checkout"
+alias d="git diff"
+alias dc="git diff --cached"
+alias g="hub"
+alias e="emacs -nw"
+alias s="git status"

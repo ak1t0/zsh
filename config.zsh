d0df73f950c341b682b8c51b8e00d92394831fa4
ak1t0 <aktoo3097@gmail.com>
2020-03-08 18:04:10 +0900

    improve git alias

--- config.zsh
+++ config.zsh
@@ -2,9 +2,15 @@ HISTSIZE=1000
 SAVEHIST=1000
 HISTFILE=~/.zsh_history
 
+# git
+alias g="git"
 alias co="git checkout"
+alias s="git status"
 alias d="git diff"
 alias dc="git diff --cached"
-alias g="hub"
+alias gp="git push origin $(git name-rev --name-only HEAD)"
+alias gc="git cherry-pick"
+# emacs
 alias e="emacs -nw"
-alias s="git status"
+
+

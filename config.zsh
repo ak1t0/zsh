bfb8ee85f6f59b4993ce2602233257220ed8714a
ak1t0 <aktoo3097@gmail.com>
2020-07-26 14:57:44 +0900

    add and clean

--- config.zsh
+++ config.zsh
@@ -4,13 +4,14 @@ HISTFILE=~/.zsh_history
 
 # git
 alias g="git"
+alias a="git add"
+alias ap="git add -p"
 alias co="git checkout"
-alias s="git status"
 alias d="git diff"
 alias dc="git diff --cached"
-alias gp="git push origin $(git name-rev --name-only HEAD)"
 alias gc="git cherry-pick"
+alias gp="git push origin $(git name-rev --name-only HEAD)"
+alias s="git status"
+
 # emacs
 alias e="emacs -nw"
-
-
